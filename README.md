882bacbb340b4baae21a436948a49a5c76cd2057
Sol Masters <teddy.masters@protonmail.com>
2022-12-14 08:41:38 -0500

    Initial code sumbission. Just code to write to the file.

---
+++ README.md
@@ -0,0 +1,7 @@
+#Daily Theme Tracking
+
+This program is designed to track your yearly theme on a daily basis. Right now it is just writing to the csv file, but I am hoping to make some visualization tools at some point.
+
+How to use:
+
+ 
---
+++ entry.py
@@ -0,0 +1,20 @@
+#import dependicies
+from datetime import date
+import csv
+#bring in the csv file
+with open("progress.csv", "w+") as csvfile:
+	#begin user inputs (must be integers)
+	print("rate all of these words. Do not input a number greater than 10 or a decimal ")
+	#rate "create" out of 10
+	create = int(input('Rate "create" out of 10 '))
+	#rate "discover" out of 10
+	discover = int(input('Rate "discover" out of 10 '))
+	#rate "work" out of 10
+	work = int(input('Rate "work" out of 10 '))
+	#rate "relationships" out of 10
+	relationships = int(input('Rate "relationships" out of 10 '))
+	#get today's date
+	date = date.today()
+	#write to csv
+	writer = csv.writer(csvfile)
+	writer.writerow([date, create, discover, work, relationships])
